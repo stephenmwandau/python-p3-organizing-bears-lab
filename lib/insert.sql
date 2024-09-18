efc34903a030b51b5c713d23af16f87cdb91827c
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-09-19 01:08:22 +0300

    final commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES
+("Mr.Chocolate", 10, "M", "Brown", "Brutal", 1),
+("Rowdy", 7, "M", "White", "Aggressive", 1),
+("Tabitha", 6, "F", "Black", "Calm", 1),
+("Sergeant Brown", 9, "M", "Gray", "Gentle", 1),
+("Melissa", 7, "F", "White", "Aggressive", 1),
+("Grinch", 8, "M", "Black", "Brutal", 1),
+("Wendy", 7, "F", "Brown", "Gentle", 1),
+(NULL, 20, "M", "Gray", "Calm", 1);
